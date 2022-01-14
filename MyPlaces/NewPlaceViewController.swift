f9c41ed0fffad8964650bb0703576cc2870e6082
javtushenko <craterec74@yandex.ru>
2022-01-14 15:17:14 +0500

    implemented MapKit

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -67,14 +67,24 @@ class NewPlaceViewController: UITableViewController {
         }
     }
     
+    // MARK: Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != "showMap" { return }
+        
+        let mapVC = segue.destination as! MapViewController
+        mapVC.place.name = placeName.text!
+        mapVC.place.location = placeLocation.text
+        mapVC.place.type = placeType.text
+        mapVC.place.imageData = placeImage.image?.pngData()
+        
+    }
+    
     func savePlace() {
         
-        var image = placeImage.image
-        if imageIsChanged == false {
-            image = UIImage(named: "imagePlaceholder")
-        }
-        
+        let image = imageIsChanged ? placeImage.image : UIImage(named: "imagePlaceholder")
         let imageData = image?.pngData()
+        
         let newPlace = Place(name: placeName.text!,
                              location: placeLocation.text,
                              type: placeType.text,
